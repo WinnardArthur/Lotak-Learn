404d9adb7b9d4083605ba6d455e61210242491c7
WinnardArthur <winnardarthur7@gmail.com>
2024-02-19 08:31:18 +0000

    add category form

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
@@ -9,6 +9,7 @@ import { IconBadge } from "@/components/icon-badge";
 import TitleForm from "./_components/title-form";
 import { DescriptionForm } from "./_components/description-form";
 import { ImageForm } from "./_components/image-form";
+import { CategoryForm } from "./_components/category-form";
 
 const CourseIdPage = async ({ params }: { params: { courseId: string } }) => {
   const { userId } = auth();
@@ -28,6 +29,12 @@ const CourseIdPage = async ({ params }: { params: { courseId: string } }) => {
   }
 
   if (!course) return redirect("/");
+
+  const categories = await db.category.findMany({
+    orderBy: {
+      name: "asc",
+    },
+  });
 
   const requiredFields = [
     course.title,
@@ -62,6 +69,14 @@ const CourseIdPage = async ({ params }: { params: { courseId: string } }) => {
           <TitleForm initialData={course} courseId={course.id} />
           <DescriptionForm initialData={course} courseId={course.id} />
           <ImageForm initialData={course} courseId={course.id} />
+          <CategoryForm
+            options={categories.map((category) => ({
+              label: category.name,
+              value: category.id,
+            }))}
+            initialData={course}
+            courseId={course.id}
+          />
         </div>
       </div>
     </div>
