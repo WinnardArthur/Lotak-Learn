bffe450561454eaafcbfa49728e7eddc1d659fcf
WinnardArthur <winnardarthur7@gmail.com>
2024-02-23 09:09:32 +0000

    change currency to ghana cedis

--- lib/format.ts
+++ lib/format.ts
@@ -1,6 +1,6 @@
 export const formatPrice = (price: number) => {
-  return new Intl.NumberFormat("en-US", {
+  return new Intl.NumberFormat("en-GH", {
     style: "currency",
-    currency: "USD",
+    currency: "GHS",
   }).format(price);
 };
