83a5c1396ad1c6b4bc2979a057698b79890cb8e5
WinnardArthur <winnardarthur7@gmail.com>
2024-02-26 08:37:03 +0000

    add course card component

--- components/course-card.tsx
+++ components/course-card.tsx
@@ -25,11 +25,13 @@ export const CourseCard = ({
 }: CourseCardProps) => {
   return (
     <Link href={`/courses/${id}`}>
-      <div className="group hover:shadow-sm transition overflow-hidden border rounded-lg p-3 h-full">
-        <div className="relative w-full aspect-video rounded-md overflow-hidden">
-          <Image fill className="object-cover" alt={title} src={imageUrl} />
+      <div className="group hover:shadow-sm transition overflow-hidden border rounded-lg h-full">
+        <div className="p-2">
+          <div className="relative w-full aspect-video rounded-md overflow-hidden">
+            <Image fill className="object-cover" alt={title} src={imageUrl} />
+          </div>
         </div>
-        <div className="flex flex-col pt-2">
+        <div className="flex flex-col p-3 pt-2">
           <div className="text-lg md:text-base font-medium group-hover:text-sky-700 transition line-clamp-2">
             {title}
           </div>
