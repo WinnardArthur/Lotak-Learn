2e5e24bf6b4295d0246c5e52c0f75a1978bd47e3
WinnardArthur <winnardarthur7@gmail.com>
2024-02-26 08:38:00 +0000

    add course list

--- app/(dashboard)/(routes)/search/page.tsx
+++ app/(dashboard)/(routes)/search/page.tsx
@@ -1,13 +1,34 @@
-import { db } from "@/lib/db";
+import { auth } from "@clerk/nextjs";
 import React from "react";
+
+import { db } from "@/lib/db";
 import { Categories } from "./_categories/categories";
 import SearchInput from "@/components/search-input";
+import { getCourses } from "@/actions/get-courses";
+import { redirect } from "next/navigation";
+import { CoursesList } from "@/components/courses-list";
 
-const SearchPage = async () => {
+interface searchPageProps {
+  searchParams: {
+    title: string;
+    categoryId: string;
+  };
+}
+
+const SearchPage = async ({ searchParams }: searchPageProps) => {
+  const { userId } = auth();
+
+  if (!userId) return redirect("/");
+
   const categories = await db.category.findMany({
     orderBy: {
       name: "asc",
     },
+  });
+
+  const courses = await getCourses({
+    userId,
+    ...searchParams,
   });
 
   return (
@@ -15,8 +36,9 @@ const SearchPage = async () => {
       <div className="px-6 pt-6 md:hidden md:mb-0 block">
         <SearchInput />
       </div>
-      <div className="p-6">
+      <div className="p-6 space-y-4">
         <Categories items={categories} />
+        <CoursesList items={courses} />
       </div>
     </>
   );
