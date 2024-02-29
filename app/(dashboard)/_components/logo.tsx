f5fb286992c17c00478f48f3013236356f5d99b2
WinnardArthur <winnardarthur7@gmail.com>
2024-02-29 09:29:55 +0000

    make logo a component

--- app/(dashboard)/_components/logo.tsx
+++ app/(dashboard)/_components/logo.tsx
@@ -1,12 +1,13 @@
 import React from "react";
 import Image from "next/image";
+import Link from "next/link";
 
 const Logo = () => {
   return (
-    <div className="flex items-center space-x-2">
+    <Link href="/" className="flex items-center space-x-2">
       <Image src="/logo.png" height={50} width={50} alt="logo" />
       <h3 className="font-extrabold text-xl">Lotak Learn</h3>
-    </div>
+    </Link>
   );
 };
 
