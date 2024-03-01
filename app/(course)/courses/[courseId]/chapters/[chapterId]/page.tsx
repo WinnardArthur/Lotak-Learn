f637e35675bc4dd4e143c6083078a018a9bac82d
WinnardArthur <winnardarthur7@gmail.com>
2024-03-01 21:40:04 +0000

    add course progress button

--- app/(course)/courses/[courseId]/chapters/[chapterId]/page.tsx
+++ app/(course)/courses/[courseId]/chapters/[chapterId]/page.tsx
@@ -9,6 +9,7 @@ import VideoPlayer from "./_components/video-player";
 import CourseEnrollButton from "./_components/course-enroll-button";
 import { Preview } from "@/components/preview";
 import { File } from "lucide-react";
+import CourseProgressButton from "./_components/course-progress-button";
 
 const ChapterIdPage = async ({
   params,
@@ -68,7 +69,12 @@ const ChapterIdPage = async ({
           <div className="p-4 flex flex-col md:flex-row items-center justify-between">
             <h2 className="text-2xl font-semibold mb-2">{chapter.title}</h2>
             {purchase ? (
-              <div>Progress button</div>
+              <CourseProgressButton
+                chapterId={params.chapterId}
+                courseId={params.courseId}
+                nextChapterId={nextChapter?.id}
+                isCompleted={!!userProgress?.isCompleted}
+              />
             ) : (
               <CourseEnrollButton
                 courseId={params.courseId}
@@ -90,7 +96,7 @@ const ChapterIdPage = async ({
                       target="_black"
                       href={attachment.url}
                       key={attachment.id}
-                      className='flex items-center p-3 w-full bg-sky-200 border text-sky-700 rounded-md hover:underline'
+                      className="flex items-center p-3 w-full bg-sky-200 border text-sky-700 rounded-md hover:underline"
                     >
                       <File />
                       <p className="line-clamp-1">{attachment.name}</p>
